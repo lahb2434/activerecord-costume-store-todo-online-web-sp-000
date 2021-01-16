42cd3bddc1abc13ce162bb621c360cbfcce753fd
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-16 17:56:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,8 +9,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.time :opening_time
-      t.time :closing_time
+      t.timestamp :opening_time
+      t.timestamp :closing_time
       
     end
   end
