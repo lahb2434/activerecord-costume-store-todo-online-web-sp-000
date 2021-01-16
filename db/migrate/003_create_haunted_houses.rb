a76702853d6c862e963d047e611534badbeb4c58
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-16 17:50:25 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 
-class HauntedHouse < ActiveRecord::Migration[5.2]
+class HauntedHouses < ActiveRecord::Migration[5.2]
   
   def change 
     create_table :haunted_house do |t|
