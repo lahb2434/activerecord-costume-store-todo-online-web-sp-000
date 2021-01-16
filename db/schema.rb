130d87d0d17c026eed231a6de373ff3c0e0d33ac
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-16 17:39:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.time "opening_time"
+    t.time "closing_time"
+  end
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "size"
+    t.string "image_url"
+  end
+
+  create_table "haunted_house", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendlty"
+    t.date "opening_date"
+    t.date "closing_date"
+    t.text "description", limit: 4294967295
+  end
 
 end
